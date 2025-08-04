4e88f0c755ca57f88084f89f66d79afb2ee00163
Atesoide <josue.naranja@gmail.com>
2025-08-03 19:24:38 -0600

    [CORRIGE] aspectos técnicos

---
+++ Soduana/Assets/Scripts_de_control/soporte/aspecto.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct aspecto
+{
+    public Material[] brands;
+    public Material[] liquidos;
+
+    public void cambiarApariencia(int seleccion) { 
+
+    }
+}
